4fcb3be8a6df36e831dfa91ded44d3d7cbfa764c
jbodet2019 <jonathan.bodet2019@campus-eni.fr>
2019-08-27 09:31:36 +0200

    Ajout des entités Site / Sortie

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SortieRepository")
+ */
+class Sortie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateDebut;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateCloture;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbInscriptionsMax;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $descriptionInfos;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $etatSortie;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $urlPhoto;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $organisateur;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $lieuNoLieu;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $etatNoEtat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDateCloture(): ?\DateTimeInterface
+    {
+        return $this->dateCloture;
+    }
+
+    public function setDateCloture(\DateTimeInterface $dateCloture): self
+    {
+        $this->dateCloture = $dateCloture;
+
+        return $this;
+    }
+
+    public function getNbInscriptionsMax(): ?int
+    {
+        return $this->nbInscriptionsMax;
+    }
+
+    public function setNbInscriptionsMax(int $nbInscriptionsMax): self
+    {
+        $this->nbInscriptionsMax = $nbInscriptionsMax;
+
+        return $this;
+    }
+
+    public function getDescriptionInfos(): ?string
+    {
+        return $this->descriptionInfos;
+    }
+
+    public function setDescriptionInfos(string $descriptionInfos): self
+    {
+        $this->descriptionInfos = $descriptionInfos;
+
+        return $this;
+    }
+
+    public function getEtatSortie(): ?int
+    {
+        return $this->etatSortie;
+    }
+
+    public function setEtatSortie(int $etatSortie): self
+    {
+        $this->etatSortie = $etatSortie;
+
+        return $this;
+    }
+
+    public function getUrlPhoto(): ?string
+    {
+        return $this->urlPhoto;
+    }
+
+    public function setUrlPhoto(?string $urlPhoto): self
+    {
+        $this->urlPhoto = $urlPhoto;
+
+        return $this;
+    }
+
+    public function getOrganisateur(): ?int
+    {
+        return $this->organisateur;
+    }
+
+    public function setOrganisateur(int $organisateur): self
+    {
+        $this->organisateur = $organisateur;
+
+        return $this;
+    }
+
+    public function getLieuNoLieu(): ?int
+    {
+        return $this->lieuNoLieu;
+    }
+
+    public function setLieuNoLieu(int $lieuNoLieu): self
+    {
+        $this->lieuNoLieu = $lieuNoLieu;
+
+        return $this;
+    }
+
+    public function getEtatNoEtat(): ?int
+    {
+        return $this->etatNoEtat;
+    }
+
+    public function setEtatNoEtat(int $etatNoEtat): self
+    {
+        $this->etatNoEtat = $etatNoEtat;
+
+        return $this;
+    }
+}
