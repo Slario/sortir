44f98ba5a6cce9e8ca3930a71658cd522331a489
Jonathan <jonathan.bodet2019@campus-eni.fr>
2019-08-27 18:28:06 +0200

    INIT

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function home(EntityManagerInterface $entityManager)
+    {
+        return $this->render("base.html.twig");
+    }
+}
