2d749ca04e8cb8e3b69fb17105f0a80a8d91643f
jpasco2019 <jordan.pasco2019@campus-eni.fr>
2019-08-27 09:30:36 +0200

    Entity Etat, Inscription, Lieu + Repository

---
+++ src/Entity/Inscription.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\InscriptionRepository")
+ */
+class Inscription
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateInscription;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idSortie;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idParticipant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->dateInscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $dateInscription): self
+    {
+        $this->dateInscription = $dateInscription;
+
+        return $this;
+    }
+
+    public function getIdSortie(): ?int
+    {
+        return $this->idSortie;
+    }
+
+    public function setIdSortie(int $idSortie): self
+    {
+        $this->idSortie = $idSortie;
+
+        return $this;
+    }
+
+    public function getIdParticipant(): ?int
+    {
+        return $this->idParticipant;
+    }
+
+    public function setIdParticipant(int $idParticipant): self
+    {
+        $this->idParticipant = $idParticipant;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Lieu.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LieuRepository")
+ */
+class Lieu
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $nomLieu;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $rue;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $latitude;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $longitude;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idVille;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomLieu(): ?string
+    {
+        return $this->nomLieu;
+    }
+
+    public function setNomLieu(string $nomLieu): self
+    {
+        $this->nomLieu = $nomLieu;
+
+        return $this;
+    }
+
+    public function getRue(): ?string
+    {
+        return $this->rue;
+    }
+
+    public function setRue(string $rue): self
+    {
+        $this->rue = $rue;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?int
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(int $latitude): self
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?int
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(int $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getIdVille(): ?int
+    {
+        return $this->idVille;
+    }
+
+    public function setIdVille(int $idVille): self
+    {
+        $this->idVille = $idVille;
+
+        return $this;
+    }
+}
